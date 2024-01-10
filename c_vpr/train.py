e54d53048afc6dfcc250cbe806e8444439c20f04
Clement Bonnet <clement.bonnet16@gmail.com>
2024-01-10 09:22:31 +0100

    exp: seq length 1000

--- c_vpr/train.py
+++ c_vpr/train.py
@@ -262,24 +262,48 @@ if __name__ == "__main__":
     run_exp(
         train_num_hops=3,
         eval_num_hops=[1, 5, 10, 20, 30, 40],
-        seq_length=100,
+        seq_length=1000,
         num_layers=1,
         num_iterations=10_000,
-        run_name="diff: 3-100, num_layers: 1",
+        run_name="diff: 3-1000, num_layers: 1",
     )
     run_exp(
         train_num_hops=3,
         eval_num_hops=[1, 5, 10, 20, 30, 40],
-        seq_length=100,
+        seq_length=1000,
         num_layers=2,
         num_iterations=10_000,
-        run_name="diff: 3-100, num_layers: 2",
+        run_name="diff: 3-1000, num_layers: 2",
     )
     run_exp(
         train_num_hops=3,
         eval_num_hops=[1, 5, 10, 20, 30, 40],
-        seq_length=100,
+        seq_length=1000,
         num_layers=3,
         num_iterations=10_000,
-        run_name="diff: 3-100, num_layers: 3",
+        run_name="diff: 3-1000, num_layers: 3",
+    )
+    run_exp(
+        train_num_hops=3,
+        eval_num_hops=[1, 5, 10, 20, 30, 40],
+        seq_length=1000,
+        num_layers=1,
+        num_iterations=10_000,
+        run_name="diff: 30-1000, num_layers: 1",
+    )
+    run_exp(
+        train_num_hops=3,
+        eval_num_hops=[1, 5, 10, 20, 30, 40],
+        seq_length=1000,
+        num_layers=2,
+        num_iterations=10_000,
+        run_name="diff: 30-1000, num_layers: 2",
+    )
+    run_exp(
+        train_num_hops=3,
+        eval_num_hops=[1, 5, 10, 20, 30, 40],
+        seq_length=1000,
+        num_layers=3,
+        num_iterations=10_000,
+        run_name="diff: 30-1000, num_layers: 3",
     )
